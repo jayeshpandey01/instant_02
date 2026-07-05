26fc36668435bd5768e556d85c5991eef0faa787
jayeshpandey01 <jayeshpandey754@gmail.com>
2026-07-06 01:46:27 +0530

    update

--- fix.py
+++
@@ -1,6 +0,0 @@
-import os
-for f, bp in [('src/routes/api.py', '@api_bp.route'), ('src/routes/blog.py', '@blog_bp.route'), ('src/routes/views.py', '@views_bp.route')]:
-    with open(f, 'r', encoding='utf-8') as file:
-        data = file.read()
-    with open(f, 'w', encoding='utf-8') as file:
-        file.write(data.replace('@app.route', bp))
